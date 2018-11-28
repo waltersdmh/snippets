1b7443faba46a1b743e56157fe4b7b026c388482
waltersdmh <wdan96@gmail.com>
2018-11-28 10:52:27 +0000

    Update coolJS.js

--- coolJS.js
+++ coolJS.js
@@ -18,3 +18,25 @@ function saveToastr(){
             }
             toastr["success"]("Cycle Saved", "Save")
         }
+
+
+function customToastr(type, text, title){
+        toastr.options = {
+            "closeButton": true,
+            "debug": false,
+            "newestOnTop": false,
+            "progressBar": true,
+            "positionClass": "toast-top-right",
+            "preventDuplicates": false,
+            "onclick": null,
+            "showDuration": "300",
+            "hideDuration": "1000",
+            "timeOut": "5000",
+            "extendedTimeOut": "1000",
+            "showEasing": "swing",
+            "hideEasing": "linear",
+            "showMethod": "fadeIn",
+            "hideMethod": "fadeOut"
+            }
+            toastr[type](text, title)
+        }
